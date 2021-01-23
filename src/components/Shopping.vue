e1618d5475fd0ada9c2dc5b581132a4b0860e0b7
王茂己 <wangmaoji@wangmaojideMacBook-Pro.local>
2021-01-23 16:32:09 +0800

    新增刪除功能

--- src/components/Shopping.vue
+++ src/components/Shopping.vue
@@ -9,23 +9,22 @@
             <input class='price'/>
             <span>數量</span>
             <input class='number'/>
-            <button v-on:click="test" class='add-btn'>新增</button>
+            <button v-on:click="addItem" class='add-btn'>新增</button>
         </div>
     </div>
     <div class='list'>
         <h3>購物清單</h3>
         <div class='shopping-list'>
-            <div v-for='item in list'
-                 v-bind:key='item.id'
-            >
-                <span>名稱：{{item.name}}</span>
-                <span>單價：{{item.price}}</span>
-                <span>數量：{{item.number}}</span>
-                <button>刪除</button>
-            </div>            
+          <div v-for='item in list'
+               v-bind:key='item.id'
+          >
+            <span>{{item.id}}. </span>
+            <span>名稱：{{item.name}}</span>
+            <span>單價：{{item.price}}</span>
+            <span>數量：{{item.number}}</span>
+            <button v-on:click="deleteItem(item.id)">刪除</button>
+          </div>            
         </div>
-        
-
     </div>
     <div class='total'>
         <h3>總價: </h3>
@@ -35,33 +34,35 @@
 </template>
 
 <script>
-let list = [];
 export default {
-  
   name: 'shopping',
   data() {
     return {
-        list: list
+      num: 1,
+      list: []
     }
+    
   },
 
   methods: {
+    addItem() {
+        let name = document.querySelector('.name').value
+        let price = document.querySelector('.price').value
+        let number = document.querySelector('.number').value
 
-        test: () => {
-            let name = document.querySelector('.name').value
-            let price = document.querySelector('.price').value
-            let number = document.querySelector('.number').value
-    
-            console.log(name , price, number)
-            list.push({
-                name, 
-                price,
-                number
-            })
-            console.log(list)
-            
-        }
-    
+        console.log(name, price, number)
+        this.list.push({
+            id: this.num,
+            name, 
+            price,
+            number
+        })
+
+        this.num ++ ;
+    },
+    deleteItem(id) {
+      this.list = this.list.filter(item => item.id !== id)
+    }
   },
   
   
