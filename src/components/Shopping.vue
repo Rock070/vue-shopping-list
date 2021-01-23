ae125e9770c66e433ac7fd8822d4c136dc35f307
王茂己 <wangmaoji@wangmaojideMacBook-Pro.local>
2021-01-23 16:49:34 +0800

    vue 語法糖使用、用 ref 拿 input 資料

--- src/components/Shopping.vue
+++ src/components/Shopping.vue
@@ -4,25 +4,37 @@
         <h3>新增單筆</h3>
         <div class='add-data' >
             <span>名稱</span>
-            <input class='name'/>
+            <input class='name'
+              ref='name'
+            />
             <span>單價</span>
-            <input class='price'/>
+            <input class='price'
+              ref='price'
+            />
             <span>數量</span>
-            <input class='number'/>
-            <button v-on:click="addItem" class='add-btn'>新增</button>
+            <input class='number'
+              ref='number'
+            />
+            <button 
+              class='add-btn'
+              @click="addItem" 
+            >新增</button>
         </div>
     </div>
     <div class='list'>
         <h3>購物清單</h3>
         <div class='shopping-list'>
           <div v-for='item in list'
-               v-bind:key='item.id'
+               :key='item.id'
           >
             <span>{{item.id}}. </span>
             <span>名稱：{{item.name}}</span>
             <span>單價：{{item.price}}</span>
             <span>數量：{{item.number}}</span>
-            <button v-on:click="deleteItem(item.id)">刪除</button>
+            <button 
+              @click="deleteItem(item.id)"
+            >
+              刪除</button>
           </div>            
         </div>
     </div>
@@ -46,9 +58,9 @@ export default {
 
   methods: {
     addItem() {
-        let name = document.querySelector('.name').value
-        let price = document.querySelector('.price').value
-        let number = document.querySelector('.number').value
+        let name = this.$refs.name.value
+        let price = this.$refs.price.value
+        let number = this.$refs.number.value
 
         console.log(name, price, number)
         this.list.push({
@@ -62,6 +74,10 @@ export default {
     },
     deleteItem(id) {
       this.list = this.list.filter(item => item.id !== id)
+    },
+    test(){
+      console.log(123)
+      console.log(this.$refs.number)
     }
   },
   computed: {
